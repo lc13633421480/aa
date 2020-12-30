86aa5af703ed3297b4d16cc4a5a1b31929eca401
lc13633421480 <13633421480@163.com>
2020-12-30 17:02:54 +0800

    一一

---
+++ BBoxTeam-master/app/src/main/java/com/example/boxteam/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.boxteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
